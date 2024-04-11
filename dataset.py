706cf0a5e815c8e153ef5602099fe5bf88e1b0d7
dg_20104 <amitgupta@iign.ac.in>
2024-04-11 14:59:09 +0530

    Initial Commit

---
+++ dataset.py
@@ -0,0 +1,72 @@
+import os
+import requests
+import json
+from bs4 import BeautifulSoup
+
+# Second Section: Declare important variables
+google_image = "https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&"
+
+user_agent = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+}
+
+# Third Section: Build the main function
+saved_folder = 'images'
+
+
+def main():
+    if not os.path.exists(saved_folder):
+        os.mkdir(saved_folder)
+    download_images()
+
+
+# Fourth Section: Build the download function
+def download_images():
+    data = input('What are you looking for? ')
+    n_images = int(input('How many images do you want? '))
+
+    print('searching...')
+
+    search_url = google_image + 'q=' + data
+    print(search_url)
+
+    response = requests.get(search_url, headers=user_agent)
+    print(response)
+
+    html = response.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    results = soup.findAll('div', {'class': 'YQ4gaf'}, limit=n_images)
+
+    for result in results:
+        text = result.text
+        print(text)
+
+    count = 1
+    links = []
+    for result in results:
+        try:
+            link = result['data-src']
+            links.append(link)
+            count += 1
+            if(count > n_images):
+                break
+
+        except KeyError:
+            continue
+
+    print(f"Downloading {len(links)} images...")
+
+    for i, link in enumerate(links):
+        response = requests.get(link)
+
+        image_name = saved_folder + '/' + data + str(i+1) + '.jpg'
+
+        with open(image_name, 'wb') as fh:
+            fh.write(response.content)
+
+
+# Fifth Section: Run your code
+if __name__ == "__main__":
+    main()
